bd19dadf22ec0099af3ddf7d93bca5119968fd6d
priyamaddheshiya <127728894+priyamaddheshiya@users.noreply.github.com>
2024-07-14 11:22:58 +0530

    new commit

--- index.js
+++ index.js
@@ -105,8 +105,8 @@ app.get('/', async (req, res) => {
 
 const ipAddress = '192.168.205.211'
 
-app.listen(port,ipAddress, () => {
-    console.log(`server is listen on the port on  http://${ipAddress}:${port}`)
+app.listen(port, () => {
+    console.log(`server is listen on the port on  http://localhost:${port}`)
 })
 //172.19.224.1
 
